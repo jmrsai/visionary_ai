899f58fc3920cdedc2c13d9662572afd9480e1d4
Jmr Sai <jmrsai987@gmail.com>
2026-01-06 02:41:16 +0000

    08:03:54.455 Running build in Washington, D.C., USA (East) – iad1 08:03:

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,7 +2,7 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 import { enableFirebaseTelemetry } from '@genkit-ai/firebase';
 
-enableFirebaseTelemetry();
+// enableFirebaseTelemetry();
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
